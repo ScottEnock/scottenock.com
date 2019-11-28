c56b7125c1a0f397e540250d8e6dd9c43350dbf2
ScottEnock <scottrowlandenock23@gmail.com>
2019-11-28 20:01:22 +0000

    Added custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function contact() {
+	return (
+		<Layout>
+			<h1>Uh, oh... You've hit a 404 error.</h1>
+            <p>This means the page your trying to find doesn't exist or has been moved...</p>
+		</Layout>
+	)
+}
